442b48e5d1ec6099402c81bce7eaccd3a5d10074
adrianhhd <118419967+adrianhhd@users.noreply.github.com>
2023-06-07 07:22:37 +0700

    Update: remove charjs

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -3,19 +3,19 @@
 
           <!-- Page Heading -->
           <div class="d-sm-flex align-items-center justify-content-between mb-4">
-            <h1 class="h3 mb-0 text-gray-800"><?php echo $title?></h1>
+            <h1 class="h3 mb-0 text-gray-800"><?php echo $title ?></h1>
 
             <div id="date"></div>
-              <script>
-                var months = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'];
-                var date = new Date();
-                var day = date.getDate();
-                var month = date.getMonth();
-                var year = date.getFullYear()
-                 
-                document.getElementById("date").innerHTML =" " + day + " " + months[month] + " " + year;
-              </script>
-            </div>
+            <script>
+              var months = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'];
+              var date = new Date();
+              var day = date.getDate();
+              var month = date.getMonth();
+              var year = date.getFullYear()
+
+              document.getElementById("date").innerHTML = " " + day + " " + months[month] + " " + year;
+            </script>
+          </div>
 
           <!-- Content Row -->
           <div class="row">
@@ -27,7 +27,7 @@
                   <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
                       <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Data Pegawai</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $pegawai?></div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $pegawai ?></div>
                     </div>
                     <div class="col-auto">
                       <i class="fas fa-users fa-2x text-gray-300"></i>
@@ -44,7 +44,7 @@
                   <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
                       <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Data Admin</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $admin?></div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $admin ?></div>
                     </div>
                     <div class="col-auto">
                       <i class="fas fa-user-cog fa-2x text-gray-300"></i>
@@ -63,7 +63,7 @@
                       <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Data Jabatan</div>
                       <div class="row no-gutters align-items-center">
                         <div class="col-auto">
-                          <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800"><?php echo $jabatan?></div>
+                          <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800"><?php echo $jabatan ?></div>
                         </div>
                       </div>
                     </div>
@@ -82,7 +82,7 @@
                   <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
                       <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Data Kehadiran</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $kehadiran?></div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $kehadiran ?></div>
                     </div>
                     <div class="col-auto">
                       <i class="fas fa-comments fa-2x text-gray-300"></i>
@@ -93,43 +93,43 @@
             </div>
           </div>
 
-          <div class="row">
+          <!-- <div class="row"> -->
 
-            <!-- Area Chart -->
-            <div class="col-xl-8 col-lg-7">
-              <div class="card shadow mb-4">
-                <!-- Card Header - Dropdown -->
-                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+          <!-- Area Chart -->
+          <!-- <div class="col-xl-8 col-lg-7"> -->
+          <!-- <div class="card shadow mb-4"> -->
+          <!-- Card Header - Dropdown -->
+          <!-- <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                   <h6 class="m-0 font-weight-bold text-primary">Data Pegawai Berdasarkan Jenis Kelamin</h6>
                   <div class="dropdown no-arrow">
                     <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                       <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
                     </a>
                   </div>
-                </div>
-                <!-- Card Body -->
-                <div class="card-body">
+                </div> -->
+          <!-- Card Body -->
+          <!-- <div class="card-body">
                   <div class="chart-bar">
                     <canvas id="myBarChart"></canvas>
                   </div>
-                </div>
-              </div>
-            </div>
+                </div> -->
+          <!-- </div> -->
+          <!-- </div> -->
 
-            <!-- Pie Chart -->
-            <div class="col-xl-4 col-lg-5">
-              <div class="card shadow mb-4">
-                <!-- Card Header - Dropdown -->
-                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+          <!-- Pie Chart -->
+          <!-- <div class="col-xl-4 col-lg-5"> -->
+          <!-- <div class="card shadow mb-4"> -->
+          <!-- Card Header - Dropdown -->
+          <!-- <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                   <h6 class="m-0 font-weight-bold text-primary">Status Pegawai</h6>
                   <div class="dropdown no-arrow">
                     <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                       <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
                     </a>
                   </div>
-                </div>
-                <!-- Card Body -->
-                <div class="card-body">
+                </div> -->
+          <!-- Card Body -->
+          <!-- <div class="card-body">
                   <div class="chart-pie pt-4 pb-2">
                     <canvas id="myPieChart"></canvas>
                   </div>
@@ -141,13 +141,13 @@
                       <i class="fas fa-circle text-success"></i> Karyawan Iidak Tetap
                     </span>
                   </div>
-                </div>
-              </div>
-            </div>
-          </div>
+                </div> -->
+          <!-- </div> -->
+          <!-- </div> -->
+          <!-- </div> -->
+
+          <!-- </div> -->
+          <!-- /.container-fluid -->
 
         </div>
-        <!-- /.container-fluid -->
-
-      </div>
-      <!-- End of Main Content -->
+        <!-- End of Main Content -->
